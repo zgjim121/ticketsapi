40507e7738032c410722756710753a81ea3dc89e
Zgjim Neziraj <=>
2024-07-12 17:13:25 +0200

    Writing Filters

--- app/Http/Resources/V1/TicketResource.php
+++ app/Http/Resources/V1/TicketResource.php
@@ -41,7 +41,7 @@ class TicketResource extends JsonResource
                     ]
                 ]
             ],
-            'includes' => new UserResource($this->whenLoaded('user')),
+            'includes' => new UserResource($this->whenLoaded('author')),
             'links' => [
                 'self' => route('tickets.show', ['ticket' => $this->id])
             ]
--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,16 +2,23 @@
 
 namespace App\Models;
 
+use App\Http\Filters\V1\QueryFilter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Builder;
 
 class Ticket extends Model
 {
     use HasFactory;
 
-    public function user(): BelongsTo
+    public function author(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function scopeFilter(Builder $builder, QueryFilter $filters)
+    {
+        return $filters->apply($builder);
     }
 }
